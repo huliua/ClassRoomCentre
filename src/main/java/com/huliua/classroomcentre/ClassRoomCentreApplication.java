4ebadf5931b38fc486cf32cbbbb61411223be3ae
huliua <1148247580@qq.com>
2025-08-18 20:40:45 +0800

    first commit

---
+++ src/main/java/com/huliua/classroomcentre/ClassRoomCentreApplication.java
@@ -0,0 +1,13 @@
+package com.huliua.classroomcentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRoomCentreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassRoomCentreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huliua/classroomcentre/ClassRoomCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huliua.classroomcentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRoomCentreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d05e316eb7d754084a9399eef40ac3810b7dd55c
huliua <1148247580@qq.com>
2025-09-20 22:25:12 +0800

    新增jasypt加密配置文件中的敏感信息

--- src/main/java/com/huliua/classroomcentre/controller/TestController.java
+++ src/main/java/com/huliua/classroomcentre/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.huliua.classroomcentre.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Value("${test.encodePassword}")
+    private String password;
+    @Value("${test.plainPassword}")
+    private String plainPassword;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
     }
+
+    @GetMapping("/getPassword")
+    public String getPassword() {
+        return "plainPassword:" + plainPassword + "\t encodePassword:" + password;
+    }
+
 }
--- src/test/java/com/huliua/classroomcentre/ClassRoomCentreApplicationTests.java
+++ src/test/java/com/huliua/classroomcentre/ClassRoomCentreApplicationTests.java
@@ -1,13 +1,25 @@
 package com.huliua.classroomcentre;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClassRoomCentreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
+    @Test
+    void testEncrypt() {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        /*配置文件中配置如下的算法*/
+        standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
+        /*配置文件中配置的password*/
+        standardPBEStringEncryptor.setPassword("huliua");
+        //加密
+        String jasyptPasswordEN = standardPBEStringEncryptor.encrypt("123456");
+        //解密
+        String jasyptPasswordDE = standardPBEStringEncryptor.decrypt(jasyptPasswordEN);
+        System.out.println("加密后密码：" + jasyptPasswordEN);
+        System.out.println("解密后密码：" + jasyptPasswordDE);
+    }
 }
